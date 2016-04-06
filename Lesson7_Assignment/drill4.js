3472dc691b8138f92ea73b4c64ccba2d6abbce39
smk13g <smk13g@my.fsu.edu>
2016-04-06 17:23:02 +0000

    Lesson 7 final commit

--- Lesson7_Assignment/drill4.js
+++ Lesson7_Assignment/drill4.js
@@ -20,3 +20,12 @@ function display2(event) {
 }//end of display2
 //attach event listener to h3 elements to invoke display function when clicked
 $("h3").click(display2);
+
+//change the background color h3 element when mouse hovers over it
+$("h3").hover(function(){
+ $(this).css("background-color", "yellow");
+ }, function(){
+ $(this).css("background-color", "pink");
+});
+//hover() will trigger display2 method each time mouse hovers over header
+$("h3").hover(display2);
